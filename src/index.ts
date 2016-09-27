9313c394c54d6f8f2eca4180adb38f8f01e4f2e9
Eduardo Nunes Pereira <eduardonunesp@gmail.com>
2016-09-26 22:58:31 -0300

    First commit

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+/// <reference path="../node_modules/phaser/typescript/phaser.d.ts"/>
+/// <reference path="../node_modules/phaser/typescript/pixi.d.ts"/>
+
+import 'pixi';
+import 'p2';
+import * as Phaser from 'phaser';
+
+class SimpleGame {
+  game: Phaser.Game;
+  logo: Phaser.Sprite;
+  cursors: Phaser.CursorKeys;
+
+  constructor() {
+    this.game = new Phaser.Game(800, 600, Phaser.AUTO, "content", this);
+  }
+
+  preload() {
+    this.game.load.image("logo", "./assets/images/mushroom2.png");
+  }
+
+  create() {
+    this.logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, "logo");
+    this.logo.anchor.setTo(0.5, 0.5);
+    this.cursors = this.game.input.keyboard.createCursorKeys();
+  }
+
+  update() {
+    this.game.input.update();
+
+    if (this.cursors.down.isDown)
+      this.logo.position.y += 10;
+    if (this.cursors.up.isDown)
+      this.logo.position.y -= 10;
+    if (this.cursors.left.isDown)
+      this.logo.position.x -= 10;
+    if (this.cursors.right.isDown)
+      this.logo.position.x += 10;
+  }
+}
+
+window.onload = () => {
+  const game = new SimpleGame();
+};
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+// Phaser webpack config
+var phaserModule = path.join(__dirname, '/node_modules/phaser/');
+var phaser = path.join(phaserModule, 'build/custom/phaser-split.js');
+var pixi = path.join(phaserModule, 'build/custom/pixi.js');
+var p2 = path.join(phaserModule, 'build/custom/p2.js');
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    pathInfo: true,
+    filename: '[name].bundle.js',
+    path: path.resolve('./dist'),
+    publicPath: '/'
+  },
+  resolve: {
+    extensions: ['', '.js', '.ts'],
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      {
+        from: './src/assets',
+        to:'./assets'
+      }
+    ]),
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      inject: 'body',
+    }),
+    new webpack.NoErrorsPlugin(),
+  ],
+  module: {
+    loaders: [
+      { test: /pixi\.js/, loader: 'expose?PIXI' },
+      { test: /phaser-split\.js$/, loader: 'expose?Phaser' },
+      { test: /p2\.js/, loader: 'expose?p2' },
+      { test: /\.ts?$/, loader: 'ts', exclude: '/node_modules/' }
+    ]
+  },
+  node: {
+    fs: 'empty'
+  },
+  resolve: {
+    alias: {
+      'phaser': phaser,
+      'pixi': pixi,
+      'p2': p2,
+    }
+  },
+  devtool: 'source-map'
+}
