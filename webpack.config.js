03ff1491c323e02c88283f2327e38273b633ee5f
Tristan W. Bonsor <twbtechnologies@gmail.com>
2017-01-27 19:10:12 -0800

    Begin refactor of webpack configuration.

--- webpack.config.js
+++ webpack.config.js
@@ -1,72 +1,14 @@
-var path = require('path');
-var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var CopyWebpackPlugin = require('copy-webpack-plugin');
-
-const ContextReplacementPlugin = require('webpack/lib/ContextReplacementPlugin');
-const CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin');
-
-// Phaser webpack config
-var phaserModule = path.join(__dirname, '/node_modules/phaser-ce/');
-var phaser = path.join(phaserModule, 'build/custom/phaser-split.js');
-var pixi = path.join(phaserModule, 'build/custom/pixi.js');
-var p2 = path.join(phaserModule, 'build/custom/p2.js');
-
-module.exports = {
-  entry: {
-    'polyfills': './src/polyfills.ts',
-    'main': './src/main.ts'
-  },
-  output: {
-    pathinfo: true,
-    filename: '[name].bundle.js',
-    path: path.resolve('./dist'),
-    publicPath: '/'
-  },
-  resolve: {
-    extensions: ['.js', '.ts'],
-    modules: ['./src', './node_modules'],
-    alias: {
-      'phaser': phaser,
-      'pixi': pixi,
-      'p2': p2,
-    }
-  },
-  plugins: [
-    new CommonsChunkPlugin({
-      name: 'polyfills',
-      chunks: ['polyfills']
-    }),
-    // Specify the correct order the scripts will be injected in.
-    new CommonsChunkPlugin({
-      name: ['polyfills']
-    }),
-    new ContextReplacementPlugin(
-      /angular(\\|\/)core(\\|\/)src(\\|\/)linker/,
-      './src',
-      {}
-    ),
-    new CopyWebpackPlugin([
-      {
-        from: './src/assets',
-        to:'./assets'
-      }
-    ]),
-    new HtmlWebpackPlugin({
-      template: './index.html',
-      inject: 'body',
-    })
-  ],
-  module: {
-    loaders: [
-      { test: /pixi\.js/, loader: 'expose-loader?PIXI' },
-      { test: /phaser-split\.js$/, loader: 'expose-loader?Phaser' },
-      { test: /p2\.js/, loader: 'expose-loader?p2' },
-      { test: /\.ts?$/, loader: 'ts-loader', exclude: '/node_modules/' }
-    ]
-  },
-  node: {
-    fs: 'empty'
-  },
-  devtool: 'source-map'
+switch (process.env.NODE_ENV) {
+  case 'prod':
+  case 'production':
+    module.exports = require('./webpack/production.config.js')({env: 'production'});
+    break;
+  case 'test':
+  case 'testing':
+    module.exports = require('./webpack/test.config.js')({env: 'test'});
+    break;
+  case 'dev':
+  case 'development':
+  default:
+    module.exports = require('./webpack/development.config.js')({env: 'development'});
 }
