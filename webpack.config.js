3a9faf1c7a7c966a79536889675eb9aaf1225bf2
Angel Daniel Munoz Gonzalez <scythes.lol@gmail.com>
2017-10-27 10:01:47 -0500

    updated config to webpack 3

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,17 @@
-var path = require('path');
-var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 // Phaser webpack config
-var phaserModule = path.join(__dirname, '/node_modules/phaser/');
-var phaser = path.join(phaserModule, 'build/custom/phaser-split.js');
-var pixi = path.join(phaserModule, 'build/custom/pixi.js');
-var p2 = path.join(phaserModule, 'build/custom/p2.js');
+const phaserModule = path.join(__dirname, '/node_modules/phaser-ce/');
+const phaser = path.join(phaserModule, 'build/phaser.js');
+const pixi = path.join(phaserModule, 'build/custom/pixi.js');
+const p2 = path.join(phaserModule, 'build/custom/p2.js');
 
 module.exports = {
   entry: './src/index.ts',
   output: {
-    pathInfo: true,
     filename: '[name].bundle.js',
     path: path.resolve('./dist'),
     publicPath: '/'
@@ -21,28 +20,27 @@ module.exports = {
     new CopyWebpackPlugin([
       {
         from: './src/assets',
-        to:'./assets'
+        to: './assets'
       }
     ]),
     new HtmlWebpackPlugin({
       template: './index.html',
       inject: 'body',
     }),
-    new webpack.NoErrorsPlugin(),
   ],
   module: {
     loaders: [
-      { test: /pixi\.js/, loader: 'expose?PIXI' },
-      { test: /phaser-split\.js$/, loader: 'expose?Phaser' },
-      { test: /p2\.js/, loader: 'expose?p2' },
-      { test: /\.ts?$/, loader: 'ts', exclude: '/node_modules/' }
+      { test: /pixi\.js/, loader: 'expose-loader?PIXI' },
+      { test: /phaser\.js$/, loader: 'expose-loader?Phaser' },
+      { test: /p2\.js/, loader: 'expose-loader?p2' },
+      { test: /\.ts?$/, loader: 'ts-loader', exclude: '/node_modules/' }
     ]
   },
   node: {
     fs: 'empty'
   },
   resolve: {
-    extensions: ['', '.js', '.ts'],
+    extensions: ['.js', '.ts'],
     alias: {
       'phaser': phaser,
       'pixi': pixi,
